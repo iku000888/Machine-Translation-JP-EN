bb9a5a40dd4d46bc0d4f967685ddff532e1990d1
iku000888 <kanumaiku@gmail.com>
2015-04-26 18:30:16 +0900

    add test for command stubs

---
+++ src/tests/connect_db.py
@@ -0,0 +1,17 @@
+import mysql.connector
+# -*- coding: utf-8 -*-
+
+cnx = mysql.connector.connect(user='root', password='root',
+                              host='127.0.0.1',
+                              database='EN_JAP')
+cursor = cnx.cursor()
+
+add_word = ("INSERT INTO word_mp "
+               "(english_word, japanese_word) "
+               "VALUES (%s, %s)")
+data_word = ('flower','花')
+
+cursor.execute(add_word,data_word)
+cnx.commit()
+cursor.close()
+cnx.close()

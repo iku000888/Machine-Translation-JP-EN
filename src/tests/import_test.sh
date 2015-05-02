fb3d95c59a2966667cbee7c21064040f0791d657
iku000888 <kanumaiku@gmail.com>
2015-05-03 00:53:23 +0900

    create test cases and test data for import/export functionality

--- src/tests/import_test.sh
+++ src/tests/import_test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #date '+%d/%m/%Y %H:%M:%S'> invocation_test.log
 python ../main.py import-words wordmp.csv
+python ../main.py import-sentences sntcmp.txt
 echo "please refer DB dump or execute export test for sanity checking.."

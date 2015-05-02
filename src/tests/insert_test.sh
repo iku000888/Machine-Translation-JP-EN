e31920d0a9703a79889543459ad0f6770a6491a5
iku000888 <kanumaiku@gmail.com>
2015-05-02 18:13:00 +0900

    add test case for word and sentence insertion

---
+++ src/tests/insert_test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#date '+%d/%m/%Y %H:%M:%S'> invocation_test.log
+python ../main.py learn-word koala コアラ
+ 
---
+++ src/tests/sentence_insert_test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#date '+%d/%m/%Y %H:%M:%S'> invocation_test.log
+python ../main.py learn-sentence "I am a boy" "私は男の子です" 
+ 

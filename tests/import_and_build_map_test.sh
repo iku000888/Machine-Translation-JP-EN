62424c08d042991914b4fc02f6cdad8fb36f835e
iku000888 <kanumaiku@gmail.com>
2015-05-18 12:50:08 +0900

    change path for executing test.

--- tests/import_and_build_map_test.sh
+++ tests/import_and_build_map_test.sh
@@ -2,8 +2,8 @@
 #date '+%d/%m/%Y %H:%M:%S'> invocation_test.log
 
 #clear DB before inserting
-../../dbscripts/connect_db.sh ../../dbscripts/create_tables.txt
-python ../main.py import-words wordmp.csv
-python ../main.py import-sentences sntcmp.txt
-python ../main.py build-sentence-word-mp 
+../dbscripts/connect_db.sh ../dbscripts/create_tables.txt
+python ../src/main.py import-words wordmp.csv
+python ../src/main.py import-sentences sntcmp.txt
+python ../src/main.py build-sentence-word-mp 
 echo "please refer DB dump or execute export test for sanity checking.."

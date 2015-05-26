615e52ee5825ee0c6c14abf9f3518b3954e221aa
iku000888 <kanumaiku@gmail.com>
2015-05-26 09:04:17 +0900

    another indentation fix. Did not cause problems.

--- src/machine_translation.py
+++ src/machine_translation.py
@@ -177,8 +177,8 @@ def sntc_having_word(word):
    cnx = get_cnx()
    cursor = cnx.cursor()
    query_sentences = ("select * from sentence_mp where " 
-                      "english_sntc like \'%" + word + "%\'" +  
-                      " or japanese_sntc like \'%" + word + "%\';")
+                 "english_sntc like \'%" + word + "%\'" +  
+                 " or japanese_sntc like \'%" + word + "%\';")
    #search_param = (word,word)
    cursor.execute(query_sentences)
    for (sid,en_sntc,jp_sntc) in cursor:

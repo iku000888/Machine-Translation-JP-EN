16cbacf07e6450ee80faa8481cb257fe72cb79e8
iku000888 <kanumaiku@gmail.com>
2015-05-27 09:25:21 +0900

    uncomment the test that was not able to complete due to regression, that is now fixed.

--- tests/export_test.sh
+++ tests/export_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #date '+%d/%m/%Y %H:%M:%S'> invocation_test.log
 python ../src/main.py export-words export_words.csv
-#python ../src/main.py export-sentences export_sntc.txt
+python ../src/main.py export-sentences export_sntc.txt
 cat export_words.csv

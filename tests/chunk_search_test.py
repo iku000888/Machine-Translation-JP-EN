5163e8db29e373663d26ecbf9cb4c5daa34d23e6
iku000888 <kanumaiku@gmail.com>
2015-05-28 09:05:01 +0900

    add support for utf8 encoding in the test

--- tests/chunk_search_test.py
+++ tests/chunk_search_test.py
@@ -1,6 +1,7 @@
+# -*- coding: utf-8 -*-
 import sys
 sys.path.append('../src/')
 import machine_translation as mc
-input_str="sur"
-print "forward=",mc.word_having_chunk(input_str)
+input_str="人"
+print mc.word_having_chunk(input_str)
 #print "reverse=",mc.random_reverse_split(input_str)

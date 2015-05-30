da59e1157bf553c4e01e10a39be43d9e134228f0
iku000888 <kanumaiku@gmail.com>
2015-05-30 21:41:35 +0900

    modify to follow PyUnit conventions. Will change the assert stmt to something that makes sense after this.

--- tests/random_splitter_test.py
+++ tests/random_splitter_test.py
@@ -1,6 +1,16 @@
+import unittest
 import sys
 sys.path.append('../src/')
 import mac_tran_utils as util
-input_str="This is a string to be split, please take a look at what happens"
-print "forward=",util.random_split(input_str)
-print "reverse=",util.random_reverse_split(input_str)
+      
+class TestRandomSplittingMethods(unittest.TestCase):
+   
+   def test_forward(self):
+      input_str="This is a string to be split,\
+            please take a look at what happens"
+      print "forward=",util.random_split(input_str)
+      print "reverse=",util.random_reverse_split(input_str)  
+      self.assertTrue(True)      
+ 
+if __name__=='__main__':
+   unittest.main()

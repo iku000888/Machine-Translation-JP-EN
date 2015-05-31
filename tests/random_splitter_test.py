93b2458c724a961e796965a81f5728cf90e4b860
iku000888 <kanumaiku@gmail.com>
2015-05-31 14:08:11 +0900

    modify the test to something more useful than blindly asserting true.

--- tests/random_splitter_test.py
+++ tests/random_splitter_test.py
@@ -4,13 +4,34 @@ sys.path.append('../src/')
 import mac_tran_utils as util
       
 class TestRandomSplittingMethods(unittest.TestCase):
-   
+   """
+   Ensure that no character is lost, 
+   or no extraneous characters are added.
+   """ 
    def test_forward(self):
       input_str="This is a string to be split,\
             please take a look at what happens"
-      print "forward=",util.random_split(input_str)
-      print "reverse=",util.random_reverse_split(input_str)  
-      self.assertTrue(True)      
- 
+      forward = util.random_split(input_str)
+      result = ""
+      for chunk in forward:
+         result += chunk
+      #print "forward=",util.random_split(input_str)
+      #print "reverse=",util.random_reverse_split(input_str)  
+      self.assertEqual(result,input_str)      
+   
+   """
+   Ensure that no character is lost, 
+   or no extraneous characters are added.
+   """
+   def test_backward(self):
+      input_str="This is a string to be split,i"\
+                "please take a look at what happens"
+      reverse=util.random_reverse_split(input_str)[::-1]
+      result = ""
+      for chunk in reverse:
+         result+=chunk
+      #print result
+      self.assertEqual(result,input_str)      
+
 if __name__=='__main__':
    unittest.main()

7296103a17c782f728d4187c6861889632ccc66e
iku000888 <kanumaiku@gmail.com>
2015-06-01 22:12:44 +0900

    half way complete unit testifying search by chunk function.

--- tests/chunk_search_test.py
+++ tests/chunk_search_test.py
@@ -9,8 +9,8 @@ class TestSearchWordFromChunk(unittest.TestCase):
    def test_chunk_srch_en(self):
       mc.delete_word_tbl()
       mc.insert_word_pair("human","人間")
-      result = mc.word_having_chunk("hu")
-      print result
+      result = mc.word_having_chunk("hu")[0]
+      #print mc.get_word_pair_by_id(int(result))
       self.assertTrue(True)
 #input_str="人"
 #print mc.word_having_chunk(input_str)

0172077fced2430f6486bca4f2889cafd0a22027
iku000888 <kanumaiku@gmail.com>
2015-06-01 09:29:38 +0900

    Start modifying to conform to PyUnit framework. Up to data insertion is done. Need to find out how to extract a single element from a set.

--- tests/chunk_search_test.py
+++ tests/chunk_search_test.py
@@ -1,7 +1,20 @@
 # -*- coding: utf-8 -*-
+import unittest
 import sys
 sys.path.append('../src/')
 import machine_translation as mc
-input_str="人"
-print mc.word_having_chunk(input_str)
+
+class TestSearchWordFromChunk(unittest.TestCase):
+   
+   def test_chunk_srch_en(self):
+      mc.delete_word_tbl()
+      mc.insert_word_pair("human","人間")
+      result = mc.word_having_chunk("hu")
+      print result
+      self.assertTrue(True)
+#input_str="人"
+#print mc.word_having_chunk(input_str)
 #print "reverse=",mc.random_reverse_split(input_str)
+
+if __name__=='__main__':
+   unittest.main()

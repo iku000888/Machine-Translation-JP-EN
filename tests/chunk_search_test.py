7111693815a35757ae19cd6d15a832b66dbd58a6
iku000888 <kanumaiku@gmail.com>
2015-06-02 13:04:03 +0900

    finish a valid test scenario - seeing whether what has been put in, can be retrieved.

--- tests/chunk_search_test.py
+++ tests/chunk_search_test.py
@@ -5,13 +5,17 @@ sys.path.append('../src/')
 import machine_translation as mc
 
 class TestSearchWordFromChunk(unittest.TestCase):
-   
    def test_chunk_srch_en(self):
       mc.delete_word_tbl()
-      mc.insert_word_pair("human","人間")
+      en_input = "human"
+      jp_input = u"人間" #need the u prefix to declare utf-8
+      mc.insert_word_pair(en_input,jp_input)
       result = mc.word_having_chunk("hu")[0]
-      #print mc.get_word_pair_by_id(int(result))
-      self.assertTrue(True)
+      retrieved_pair= mc.get_word_pair_by_id(int(result))
+      self.assertEqual(en_input, retrieved_pair[0])
+      print jp_input, retrieved_pair[1]
+      self.assertEqual(jp_input,retrieved_pair[1])
+#self.assertTrue(True)
 #input_str="人"
 #print mc.word_having_chunk(input_str)
 #print "reverse=",mc.random_reverse_split(input_str)

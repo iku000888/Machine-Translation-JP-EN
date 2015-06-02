b31069e01157a1ae50e2d38f943804e328348b7d
iku000888 <kanumaiku@gmail.com>
2015-06-02 13:06:40 +0900

    add another test case for searching with Japanese as an input.

--- tests/chunk_search_test.py
+++ tests/chunk_search_test.py
@@ -16,5 +16,15 @@ class TestSearchWordFromChunk(unittest.TestCase):
       print jp_input, retrieved_pair[1]
       self.assertEqual(jp_input,retrieved_pair[1])
 
+   def test_chunk_srch_jp(self):
+      mc.delete_word_tbl()
+      en_input = "human"
+      jp_input = u"人間" #need the u prefix to declare utf-8
+      mc.insert_word_pair(en_input,jp_input)
+      result = mc.word_having_chunk("人")[0]
+      retrieved_pair= mc.get_word_pair_by_id(int(result))
+      self.assertEqual(en_input, retrieved_pair[0])
+      print jp_input, retrieved_pair[1]
+      self.assertEqual(jp_input,retrieved_pair[1])
 if __name__=='__main__':
    unittest.main()

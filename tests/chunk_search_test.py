ec362b16c4df782c4ea4511aea110a7885443872
iku000888 <kanumaiku@gmail.com>
2015-06-03 08:53:28 +0900

    start working on more test cases

--- tests/chunk_search_test.py
+++ tests/chunk_search_test.py
@@ -24,5 +24,22 @@ class TestSearchWordFromChunk(unittest.TestCase):
       retrieved_pair= dao.get_word_pair_by_id(int(result))
       self.assertEqual(en_input, retrieved_pair[0])
       self.assertEqual(jp_input,retrieved_pair[1])
+
+   def test_chunk_srch_multiple_en(self):
+      dao.delete_word_tbl()
+      dao.insert_word_pair("living",u"生活する")
+      dao.insert_word_pair("invited",u"招待される")
+      dao.insert_word_pair("involved",u"関わる")
+      dao.insert_word_pair("indeed",u"そのとおり")
+      dao.insert_word_pair("intention",u"想定")
+      dao.insert_word_pair("initiation",u"開始")
+      dao.insert_word_pair("nicotine",u"ニコチン")
+      dao.insert_word_pair("bin",u"ゴミ箱")
+      result = dao.word_having_chunk("in")
+      print result
+      #retrieved_pair= dao.get_word_pair_by_id(int(result))
+      #self.assertEqual(en_input, retrieved_pair[0])
+      #self.assertEqual(jp_input,retrieved_pair[1])
+
 if __name__=='__main__':
    unittest.main()

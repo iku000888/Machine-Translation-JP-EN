551a513e00805a940296e3e996f49325eb56a0a9
iku000888 <kanumaiku@gmail.com>
2015-06-03 00:14:57 +0900

    finish editing the test_all script for the day.

--- tests/test_all.sh
+++ tests/test_all.sh
@@ -3,7 +3,11 @@
 
 #must be in the test directory to run the unit test
 #otherwise relative path will fail.
+
+#If necessary environment variables are not set,
+#set them.
 . ../src/set_environmet.sh
+
 mysql --user=root --password=$MYSQLPW <../dbscripts/create_tables.txt
 echo 'dropping and re-adding the tables...'
 set LOG_DEST 'test_artifacts/PyUnit.log'

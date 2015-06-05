422924fc46dacd4a5947035feaa7ccfe4099fd2d
iku000888 <kanumaiku@gmail.com>
2015-06-05 09:09:13 +0900

    remove irrelevant code block.

--- tests/translate_word_test.py
+++ tests/translate_word_test.py
@@ -36,31 +36,6 @@ class TestWordTranslation(unittest.TestCase):
       dao.insert_word_pair(en_input,jp_input)
       with self.assertRaises(TypeError):
          self.assertRaises(dao.translate_word("JP2EN",u"灰"))
-#   """
-#   Test for multiple words containing the input chunk.
-#   Ensure that what gets put in, is retrieved by verifying
-#   the number of elements.
-#   """
-#   def test_chunk_srch_multiple_en(self):
-#      dao.delete_word_tbl()
-#      pairs = dict()
-#      pairs["living"    ]=u"生活する"
-#      pairs["invited"   ]=u"招待される"
-#      pairs["involved"  ]=u"関わる"
-#      pairs["indeed"    ]=u"そのとおり"
-#      pairs["intention" ]=u"想定"
-#      pairs["initiation"]=u"開始"
-#      pairs["nicotine"  ]=u"ニコチン"
-#      pairs["bin"       ]=u"ゴミ箱"
-#      for en_word in pairs.keys():
-#         dao.insert_word_pair(en_word,pairs[en_word])
-#      list_stuff = list()
-#      result = dao.word_having_chunk("in")
-#      for wid in result:
-#         retrieved_pair=dao.get_word_pair_by_id(int(wid))
-#         list_stuff.append(retrieved_pair)
-#      self.assertEqual(len(list_stuff),8)
-#         #self.assertEqual(jp_input,retrieved_pair[1])
 
 if __name__=='__main__':
    unittest.main()

b4cd3703e9c7bf1e20b479dd4f28f512f2afaf82
iku000888 <kanumaiku@gmail.com>
2015-06-08 09:23:15 +0900

    improve the build map test case.
    Still need to implement a function to fetch a woed_id from
    the string.

--- tests/build_map_test.py
+++ tests/build_map_test.py
@@ -9,15 +9,28 @@ class TestBuildSntcWordMp(unittest.TestCase):
       dao.delete_word_tbl()
       dao.delete_sntc_tbl()
       dao.delete_sntc_wd_tbl()
-      dao.insert_word_pair(u"I",u"私")
-      dao.insert_word_pair(u"am",u"は")
-      dao.insert_word_pair(u"a",u"一人の")
-      dao.insert_word_pair(u"human",u"人間だ")
+      pairs = dict()
+      pairs[u"I"    ]=u"私"    
+      pairs[u"am"   ]=u"は"    
+      pairs[u"a"    ]=u"一人の"
+      pairs[u"human"]=u"人間だ"
+      for k,v in pairs.iteritems():
+         dao.insert_word_pair(k,v)
       dao.insert_sentence_pair(u"I am a human",
                                  u"私は一人の人間だ")
+      #build the relational mapping.
       dao.build_word_sntc_mp()
+
+      #verify that the words are reachable from the sentence.
       sntc_id = dao.get_sntc_id(u"I am a human")
-      word_list=dao.words_belonging_to_sntc(sntc_id)
+      word_ids= dao.words_belonging_to_sntc(sntc_id)
+      for k,v in pairs.iteritems():
+         self.assertTrue(True)
+      print word_ids
+
+      #verify that the sentence is reachable from the words.
+
+
 #   def test_dictionary_import(self):
 #      dao.delete_word_tbl()
 #      dao.delete_sntc_tbl()

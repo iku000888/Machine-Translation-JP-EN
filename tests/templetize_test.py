ee08bf0c687f419aade92dbedcd20dc955d78b7c
iku000888 <kanumaiku@gmail.com>
2015-06-09 19:49:21 +0900

    finish a test case for templetization.

--- tests/templetize_test.py
+++ tests/templetize_test.py
@@ -2,7 +2,7 @@
 import unittest
 import sys
 sys.path.append('../src/')
-import mac_tran_util as util
+import mac_tran_utils as util
 
 class TestSearchWordFromChunk(unittest.TestCase):
    """
@@ -11,12 +11,14 @@ class TestSearchWordFromChunk(unittest.TestCase):
    the number of elements.
    """
    def test_chunk_srch_multiple_en(self):
-      dao.delete_word_tbl()
       sntc = "I am alive today."
       pairs = dict()
       pairs[u"I"    ]="$<1>"
       pairs[u"am"   ]="$<2>"
       pairs[u"alive"]="$<3>"
       pairs[u"today"]="$<4>"
+      pairs[u"."]="$<5>"
+      returned_val = util.templetize_sentence(sntc,pairs)
+      self.assertEqual("$<1> $<2> $<3> $<4>$<5>",returned_val)
 if __name__=='__main__':
    unittest.main()

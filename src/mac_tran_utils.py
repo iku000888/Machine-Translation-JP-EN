b21deb67c51901465fba6c766439209264f9f3e1
iku000888 <kanumaiku@gmail.com>
2015-06-09 19:47:58 +0900

    remove commented out print statements.
    They will never be used again.

--- src/mac_tran_utils.py
+++ src/mac_tran_utils.py
@@ -4,22 +4,14 @@ import random
 def random_split(str_to_split):
    splitted_list = list()
    while len(str_to_split)>=1:
-      #print "loop start"
       len_chunk=1+int(.35*round(random.random()\
       *len(str_to_split)))
-      #print "lenchunk=",len_chunk
       chunk=str_to_split[0:len_chunk]
-      #print "chunk=",chunk
       splitted_list.append(chunk)
-      #print "list=",splitted_list
       str_to_split=str_to_split[len_chunk:len(str_to_split)]
-      #print str_to_split
-      #print "end of loop"
-   #print splitted_list
    return splitted_list
 
 def random_reverse_split(str_to_split):
-   #print str_to_split#[0:0:-1]
    rand_reversed_str=random_split(str_to_split[::-1])
    fix_chunks = list()
    for chunk in rand_reversed_str:
@@ -45,4 +37,3 @@ def templetize_sentence(sntc,word_dict):
    for word,param in word_dict.iteritems():
       templetized_sntc = templetized_sntc.replace(word,param)
    return templetized_sntc
-# random_split("yolo, this is a very long string. I am on a train")

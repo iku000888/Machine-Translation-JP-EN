02aa70df68b69109b52b87870a14a44796784adf
iku000888 <kanumaiku@gmail.com>
2015-06-10 20:49:26 +0900

    define skeleton and description of attempt_word_retrieval.

--- src/mac_tran_core_logic.py
+++ src/mac_tran_core_logic.py
@@ -1,3 +1,13 @@
 """
 Leveraging the dao and util, produce useful results.
 """
+
+def attempt_word_retrieval(sntc):
+   """
+   given a sentence, 'attempt' to identify the words that
+   are included in the sentence by splitting the sentence
+   into arbitrary chunks, and hoping it would partially
+   match a word.
+   """
+   word_ids = list()
+   return word_ids

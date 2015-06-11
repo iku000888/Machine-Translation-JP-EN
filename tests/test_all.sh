71383d8295f616754a5390e61eb9f181ef7b45ef
iku000888 <kanumaiku@gmail.com>
2015-06-11 09:49:35 +0900

    add description of the necessity of DB connection.

--- tests/test_all.sh
+++ tests/test_all.sh
@@ -16,9 +16,11 @@
 
 mysql --user=root --password=$MYSQLPW <../dbscripts/create_tables.txt
 echo 'dropping and re-adding the tables...'
+#from util module, which does not require DB connection
 python random_splitter_test.py
 python templetize_test.py
 
+#from dao module, which does require DB connection.
 python chunk_search_test.py
 python translate_word_test.py
 python build_map_test.py

3aee3a7b7d4f7ac5d74715bf43b7741bead4a2c1
iku000888 <kanumaiku@gmail.com>
2015-06-13 21:55:56 +0900

    replace value dumping with appropriate assertion.
    which would be asserting that all words can be retrieved.

--- tests/core_logic_test.py
+++ tests/core_logic_test.py
@@ -6,12 +6,18 @@ import mac_tran_dao as dao
 import mac_tran_core_logic as core
 
 class TestCoreLogics(unittest.TestCase):
+   
+   #assert that words contained in the given sntc
+   #can be identified, at least for most of the time.
    def test_attempt_word_retrieval(self):
       dao.delete_word_tbl()
       self.setup_data()
       w_ids=core.attempt_word_retrieval(u"I am going to\
          retrieve words that this sentence contains.")
-      print w_ids
+      #print w_ids
+      #for wid in w_ids:
+      #   print dao.get_word_pair_by_id(wid)
+      self.assertEqual(len(w_ids),11)
    def setup_data(self):
       pairs = dict()
       pairs["I"        ]=u"私"

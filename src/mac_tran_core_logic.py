b9835482d80b729c4ef86908ba3b1f8567a57cb0
iku000888 <kanumaiku@gmail.com>
2015-06-15 08:54:28 +0900

    add a comma to the comments

--- src/mac_tran_core_logic.py
+++ src/mac_tran_core_logic.py
@@ -8,7 +8,7 @@ import mac_tran_utils as util
 def attempt_word_retrieval(sntc):
    """
    given a sentence, 'attempt' to identify the words that
-   are included in the sentence by splitting the sentence
+   are included in the sentence, by splitting the sentence
    into arbitrary chunks, and hoping it would partially
    match a word.
    """

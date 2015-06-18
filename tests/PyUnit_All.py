c2f2bce1001e6d4141ecc29d2c59c3a35956ee53
iku000888 <kanumaiku@gmail.com>
2015-06-19 00:42:07 +0900

    prefix the run all test script with prompts
    to obtain db credentials.

--- tests/PyUnit_All.py
+++ tests/PyUnit_All.py
@@ -2,6 +2,16 @@
 #for the time being, need to invoke the set_environment script
 #to set up data base informations.
 import unittest
+import os
+import sys
+
+print "MYSQL ip address, please"
+os.environ["MYSQLIP"] = sys.stdin.readline()
+print "id please"
+os.environ["MYSQLID"] = sys.stdin.readline()
+print "password, please"
+os.environ["MYSQLPW"] = sys.stdin.readline()
+
 loader = unittest.TestLoader()
 mods = loader.discover('.',pattern='*test*.py',top_level_dir=None)
 suite = unittest.TestSuite(mods)

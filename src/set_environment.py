db4a61f7e08c376b9b26f53e77744796f8e074fb
iku000888 <kanumaiku@gmail.com>
2015-06-20 22:47:27 +0900

    removing set env from python as it surely will not work.
    This fails because:
     http://stackoverflow.com/questions/18577555/sourcing-a-python-script

--- src/set_environment.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-print "MYSQL ip address, please"
-os.environ["MYSQLIP"] = sys.stdin.readline()
-print "id please"
-os.environ["MYSQLID"] = sys.stdin.readline()
-print "password, please"
-os.environ["MYSQLPW"] = sys.stdin.readline()

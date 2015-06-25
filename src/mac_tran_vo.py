798241ff38ce0be0e0176123b5ed3c62d54ee58e
iku000888 <kanumaiku@gmail.com>
2015-06-25 20:33:08 +0900

    Add design direction as comments.

--- src/mac_tran_vo.py
+++ src/mac_tran_vo.py
@@ -1,4 +1,17 @@
 class MacTranVo():
+   """
+   This class should package data and access methods relavant
+   to the logic part. As of now, I am imagining the following attributes:
+      -The sentence it self.
+      -The string's meta data
+         -The detected template <i's> and mapping to the words contained in the
+            sentence
+      -List of candidate strings fetched from the DB, given by the DAO.
+      -Some fuzzy-ish logic to translate the 
+       sentence using the above items as ingredients
+
+      
+   """
    def __init__(self,string, lang):
       self.string = string
       self.lang = lang

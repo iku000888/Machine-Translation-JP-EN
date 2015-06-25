27c6fff231a831597126a560e52f7a6aa838aa81
iku000888 <kanumaiku@gmail.com>
2015-06-25 09:18:36 +0900

    re-discover object orientation with python.
    Now I do need a clean way to handle the values passed around and the
    associated meta data.

---
+++ src/mac_tran_vo.py
@@ -0,0 +1,8 @@
+class MacTranVo():
+   def __init__(self,string, lang):
+      self.string = string
+      self.lang = lang
+
+   def print_self(self):
+      print self.string
+      print self.lang

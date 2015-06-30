1f3cf92a44385c2cd7c1a3a3f04df99abbe4a580
iku000888 <kanumaiku@gmail.com>
2015-06-30 19:35:49 +0900

    add japanese validation for getter setter testing.

--- tests/mac_tran_vo_test.py
+++ tests/mac_tran_vo_test.py
@@ -7,9 +7,15 @@ import mac_tran_vo as vo
 class TestMacTranVo(unittest.TestCase):
    def test_setter_getters_en(self):
       test_vo = vo.MacTranVo()
-      test_vo.set_sentence("hello, can you hear me?")
+      test_vo.set_sentence(u"hello, can you hear me?")
       self.assertEqual(test_vo.get_sentence(),
-                        "hello, can you hear me?") 
+                        u"hello, can you hear me?") 
+   
+   def test_setter_getters_jp(self):
+      test_vo = vo.MacTranVo()
+      test_vo.set_sentence(u"こんにちは、聞こえますか?")
+      self.assertEqual(test_vo.get_sentence(),
+                        u"こんにちは、聞こえますか?") 
       
 if __name__=='__main__':
    unittest.main()
